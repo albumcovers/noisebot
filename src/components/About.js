3ccd8b880a92f4bc4bfa7b28d43879fa7e2d4c24
album covers <135176319+albumcovers@users.noreply.github.com>
2024-01-22 16:52:33 -0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <div>
               <h4 className="h-color">⚡ Commands</h4>
               <p className="p-color">
-                We have more than 200 commands support in this bot.
+                We have tons of preset characters and personalities on this bot.
               </p>
               <hr />
             </div>
@@ -38,22 +38,6 @@ const About = () => {
             Check out Commands &rarr;
           </Link>
         </p>
-
-        <section className="about-stats">
-          <div className="about-stats-card">
-            <h4 className="h-color">17k+</h4>
-            <p className="p-color">Total Servers</p>
-          </div>
-          <div className="about-stats-card">
-            <h4 className="h-color">94k+</h4>
-            <p className="p-color">Total Users</p>
-          </div>
-          <div className="about-stats-card">
-            <h4 className="h-color">375k+</h4>
-            <p className="p-color">Total Commands Ran</p>
-          </div>
-        </section>
-      </section>
     </>
   );
 };
